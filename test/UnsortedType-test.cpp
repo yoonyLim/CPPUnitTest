87360213d5867b92eb08ef79de5f46b8469eaf0f
Hayoon Lim / 임하윤 <64838255+yoonyLim@users.noreply.github.com>
2024-05-22 22:05:53 +0900

    add: sorted list test case

--- test/UnsortedType-test.cpp
+++ test/UnsortedType-test.cpp
@@ -1,12 +1,20 @@
 #include "gmock/gmock.h"
 #include "UnsortedList.h"
 
-// https://www.youtube.com/watch?v=dLB2aDasVTg&list=PL_dsdStdDXbo-zApdWB5XiF2aWpsqzV55
+/*
+ * @brief Mock class to be used for test
+ * @pre Each function is a mock method that takes in zero argument
+ */
 class MockUnsortedList: public UnsortedList {
 public:
 	MOCK_METHOD0(GetList, std::string());
+	MOCK_METHOD0(GetSortedList, std::string());
 };
 
+/*
+ * @brief Test class to utiltize mock object
+ * @pre Mock instance initializes UnsortedList member variable
+ */
 class TestUnsortedList {
 private:
 	UnsortedList& testList;
@@ -20,8 +28,15 @@ public:
 	std::string GetList() {
 		return testList.GetList();
 	}
+
+	std::string GetSortedList() {
+		return testList.GetSortedList();
+	}
 };
 
+/*
+ * @brief Google Test Case: compare unsorted list for given inputs / Test Suite: Unsorted List Test
+ */
 TEST(UnsortedListTest, CompareUnsortedListTest) {
 	MockUnsortedList mockList;
 	TestUnsortedList list(mockList);
@@ -32,4 +47,19 @@ TEST(UnsortedListTest, CompareUnsortedListTest) {
 	list.InsertItem(5);
 
 	EXPECT_EQ(list.GetList(), "5231");
+}
+
+/*
+ * @brief Google Test Case: compare sorted list for given inputs / Test Suite: Unsorted List Test
+ */
+TEST(UnsortedListTest, CompareSortedListTest) {
+	MockUnsortedList mockList;
+	TestUnsortedList list(mockList);
+
+	list.InsertItem(1);
+	list.InsertItem(3);
+	list.InsertItem(2);
+	list.InsertItem(5);
+
+	EXPECT_EQ(list.GetSortedList(), "1235");
 }
