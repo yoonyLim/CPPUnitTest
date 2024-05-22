fcd46bc802736c08bcb1e16f092d78d27ac58278
Hayoon Lim / 임하윤 <hayoon603@gmail.com>
2024-05-22 17:35:20 +0900

    feat: successful mock test

--- test/UnsortedType-test.cpp
+++ test/UnsortedType-test.cpp
@@ -1,8 +1,30 @@
-#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include "UnsortedList.h"
 
+// https://www.youtube.com/watch?v=dLB2aDasVTg&list=PL_dsdStdDXbo-zApdWB5XiF2aWpsqzV55
+class MockUnsortedList: public UnsortedList {
+public:
+	MOCK_METHOD0(GetList, std::string());
+};
+
+class TestUnsortedList {
+private:
+	UnsortedList& testList;
+public:
+	TestUnsortedList(UnsortedList& _testList) : testList(_testList) {}
+
+	void InsertItem(int num) {
+		testList.InsertItem(num);
+	}
+
+	std::string GetList() {
+		return testList.GetList();
+	}
+};
+
 TEST(UnsortedListTest, CompareUnsortedListTest) {
-	UnsortedList list;
+	MockUnsortedList mockList;
+	TestUnsortedList list(mockList);
 
 	list.InsertItem(1);
 	list.InsertItem(3);
